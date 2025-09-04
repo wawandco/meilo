a811308cfa0914a9d4b7def96a5f528013993293
Joe Arias <jarias@wawand.co>
2025-09-04 07:35:08 -0500

    feat: adding email model

---
+++ internal/models/email.go
@@ -0,0 +1,40 @@
+package models
+
+import (
+	"strings"
+	"time"
+)
+
+const (
+	textPlain = "text/plain"
+	textHtml  = "text/html"
+)
+
+type Email struct {
+	ID          int64        `json:"-"`
+	Subject     string       `json:"subject"`
+	Sender      string       `json:"sender"`
+	Recipients  []string     `json:"-"`
+	CC          []string     `json:"-"`
+	BCC         []string     `json:"-"`
+	Body        string       `json:"emailBody"`
+	Bodies      []Body       `json:"-"`
+	Attachments []Attachment `json:"-"`
+	ReceivedAt  time.Time    `json:"receivedAt"`
+}
+
+type Body struct {
+	ContentType string
+	Content     string
+}
+
+type Attachment struct {
+	Name        string
+	Path        string
+	ContentType string
+	Data        []byte
+}
+
+func (e *Email) RawRecipients() string {
+	return strings.Join(e.Recipients, ",")
+}
