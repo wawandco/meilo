4da0f5fff207e04c8fc1d17df2452e4e2f00a709
Bryan Moslo <balgutria@wawand.co>
2026-01-28 13:50:32 -0500

    adding support for the Reply-To param

--- sender.go
+++ sender.go
@@ -52,6 +52,7 @@ func send(e email) error {
 		strings.Join(e.To, ", "),
 		strings.Join(e.Cc, ", "),
 		strings.Join(e.Bcc, ", "),
+		html.EscapeString(e.ReplyTo),
 		html.EscapeString(e.Subject),
 	)
 
